a6b5abf3ac32aafe5aa40a81a300c81e7138eed1
DanielEspinel <>
2022-04-27 16:26:30 -0600

    Init NPM

--- replit.nix
+++ replit.nix
@@ -1,6 +1,8 @@
 { pkgs }: {
   deps = [
-    pkgs.nodePackages.vscode-langservers-extracted
-    pkgs.nodePackages.typescript-language-server
+    pkgs.sass
+    pkgs.nodejs-16_x
+      pkgs.nodePackages.vscode-langservers-extracted
+      pkgs.nodePackages.typescript-language-server
   ];
 }
